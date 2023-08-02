b23ca765ebc42211978dddf5b824696e2e907dfa
hedesh <hedes.hmb@gmail.com>
2023-08-02 14:14:40 +0330

    :bug: :(SessionController) Fixed bug in login auth

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
 
 class SessionsController extends Controller
 {
@@ -19,19 +21,24 @@ class SessionsController extends Controller
      */
     public function store(): \Illuminate\Foundation\Application|\Illuminate\Routing\Redirector|\Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse
     {
-        $attributes = request()->validate([
-            'email'=>'required|email',
-            'password'=>'required'
+        $inputData = request()->all();
+
+        $validator = Validator::make(request()->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
         ]);
 
-        if(Auth::attempt($attributes))
-        {
-            session()->regenerate();
-            return redirect('dashboard')->with(['success'=>'You are logged in.']);
+        if ($validator->fails()) {
+            return back()->withErrors(['email' => 'Email or password invalid.']);
         }
-        else{
 
-            return back()->withErrors(['email'=>'Email or password invalid.']);
+        if (Auth::attempt(['email' => $inputData['email'],
+            'password' => $inputData['password']])) {
+            session()->regenerate();
+            return redirect('dashboard')->with(['success' => 'You are logged in.']);
+        } else {
+
+            return back()->withErrors(['email' => 'Email or password invalid.']);
         }
     }
 
@@ -41,6 +48,6 @@ class SessionsController extends Controller
     public function destroy(): \Illuminate\Foundation\Application|\Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse|\Illuminate\Contracts\Foundation\Application
     {
         Auth::logout();
-        return redirect('/login')->with(['success'=>'You\'ve been logged out.']);
+        return redirect('/login')->with(['success' => 'You\'ve been logged out.']);
     }
 }
